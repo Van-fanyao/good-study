62c857805fdcdaa5048db43beeb86d8497bd8e27
fanyao <15972019340@163.com>
2020-05-16 10:31:41 +0800

    first commit

---
+++ src/main/java/com/good/study/GoodStudyApplication.java
@@ -0,0 +1,13 @@
+package com.good.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/good/study/GoodStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.good.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
